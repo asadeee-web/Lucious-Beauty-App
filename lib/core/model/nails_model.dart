89f16d1acf5325c70b908428045c642291d77a52
Asad Qureshi <asadeee998@gmail.com>
2024-03-20 07:41:19 +0500

    nails screen created

---
+++ lib/core/model/nails_model.dart
@@ -0,0 +1,11 @@
+class nails {
+  String? image;
+  String? title;
+  String? time;
+  String? icon;
+  nails(
+      {required this.image,
+      required this.title,
+      required this.icon,
+      required this.time});
+}
---
+++ lib/ui/screens/nails/nails_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NailScreen extends StatelessWidget {
+  const NailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ();
+  }
+}
